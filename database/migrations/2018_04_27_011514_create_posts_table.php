7287ee78d01975063c843d198ca0e41b44cd804e
aditama <arizki.aditama@gmail.com>
2018-05-08 06:13:12 +0700

    add foreign key wisata in post table

--- database/migrations/2018_04_27_011514_create_posts_table.php
+++ database/migrations/2018_04_27_011514_create_posts_table.php
@@ -16,11 +16,13 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->integer('wisata_id')->unsigned();
             $table->text('caption');
             $table->string('image');
             $table->integer('like');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('wisata_id')->references('id')->on('tempat_wisatas')->onDelete('cascade');
         });
     }
 
