70db2ef58d2d436b349c8f2079c3179aaf6c9750
kevinchou <crossmajor99@gmail.com>
2018-05-12 09:34:44 +0700

    resolve merge 2

--- resources/views/getstarted.blade.php
+++ resources/views/getstarted.blade.php
@@ -12,7 +12,7 @@
             <div class="btext">
                     <h1>Kuy Jalan</h1>
                     <p>Platform Berbagi Pengalaman liburan</p>
-                    <a href="{{ url('login') }}" class="btn">Get Started></a>
+                    <a href="{{ url('login') }}" class="btn">Get Started</a>
             </div>
     </div>
    
