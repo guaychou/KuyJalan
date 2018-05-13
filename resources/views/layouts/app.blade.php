69ebe00d255faae5a1b74c501fda2906e0241a74
yusufsaputra <yusufmualo@gmail.com>
2018-05-13 21:30:16 +0700

    tambahan kata profil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,11 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Profile') }}
+                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
