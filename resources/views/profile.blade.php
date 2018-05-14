1473adc25a06e8b146351c4841616ba8370f822f
yusufsaputra <yusufmualo@gmail.com>
2018-05-14 22:54:53 +0700

    update profil

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+@section('content')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.css">
+<scrip src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.js"/>
+<body style="background-color: #CFCCCC">
+<div class="ui grid container">
+  
+  <div class="sixteen wide column">
+  	<div class="ui items">
+  <div class="item">
+    <div class="ui small image">
+      <img src="images/foto.jpg">
+    </div>
+    <div class="content">
+      <div class="header">Yusuf Saputra</div>
+      <div class="meta">
+      	
+
+<div class="ui labeled button" tabindex="0">
+  <div class="ui blue button">
+    <i class="pointing icon"></i> Point
+  </div>
+  <a class="ui basic left pointing blue label">
+    1,048
+  </a>
+</div>
+
+      <div class="post">
+      	<div class="ui button">
+    <i class="pointing icon"></i> Post
+  </div>
+  <a class="ui basic left pointing blue label">
+    189
+  </a>
+</div> 
+
+  </div>
+  <div class="sixteen wide column">
+  	<div class="ui three stackable cards">
+  <div class="card">
+    <div class="image">
+      <img src="images/foto.jpg">
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="images/foto.jpg">
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="images/foto.jpg">
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="images/foto.jpg">
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="images/foto.jpg">
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="images/foto.jpg">
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="sixteen wide column"></div>
+</div>
+</body>
+@endsection
