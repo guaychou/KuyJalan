1821addc31c62f31cd56177ef8c18af18ab8fd3b
Insan2121 <gamersg801@gmail.com>
2018-05-14 22:19:33 +0700

    home hampir fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,32 @@
 <div style="margin-top: 50px;">
 
 @foreach ($output as $out)
-   <div>
-        <img src="{{ asset($out->image) }}"><br/>
-        {{$out->nama_tempat_wisata}}
-        {{$out->name }}<br>
-        {{$out->caption}}<br>
-        {{$out->nama_kota}}<br>
-        Like : {{$out->like}}<br>
-   </div> 
+<div class="ui centered card">
+  <div class="content">
+    <span class="floated meta">{{$out->nama_tempat_wisata}}</span>
+    <div class="right floated meta">{{$out->nama_kota}}</div>
+    <br>
+    <img class="ui avatar image" src="{{asset($out->image) }}"> {{$out->name }}
+  </div>
+  <div class="image" >
+    <img src="{{asset($out->image) }}">
+  </div>
+  <div class="content">
+    <span class="right floated">
+      <i class="heart outline like icon"></i>
+      {{$out->like}} likes
+    </span>
+    
+  </div>
+  <div class="extra content">
+    <div class="ui large transparent left icon input">
+      
+      <div class="description">
+      {{$out->caption}}
+    </div>
+    </div>
+  </div>
+</div>
     
 @endforeach
 
