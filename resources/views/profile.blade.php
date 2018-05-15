21ebfdfe6abc5d4b40a8d798ab87a3748cb995d9
yusufsaputra <yusufmualo@gmail.com>
2018-05-15 09:40:58 +0700

    tambahan profil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -16,31 +16,25 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.css">
 <scrip src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.js"/>
 <body>
-<div class="ui grid">
-  <div class="sixteen wide column" style="background-color: blue">
-    
-  </div>
-
-
+<div class="ui grid" style="margin-top: 43px">
+  
   <div class="four wide column" style="background-color: white">
+
     <div class="ui centered card">
   <div class="image">
     <img src="/images/foto.jpg">
   </div>
   <div class="content">
     <a class="header">{{ Auth::user()->name }}</a>
-    <div class="ui labeled button" tabindex="0">
-  <div class="ui blue button">
+    
+      <div class="post">
+        <div class="ui blue button">
     <i class="pointing icon"></i> Point
   </div>
   <a class="ui basic left pointing blue label">
     {{$count}}
-     </a>
-    
-    
-    
-</div>
-
+  </a>
+</div> 
       <div class="post">
         <div class="ui button">
     <i class="pointing icon"></i> Post
@@ -152,10 +146,41 @@
   </div>
 </div>
 
+ @foreach ($user as $out)
+<div class="ui centered card">
+  <div class="content">
+    <span class="floated meta">{{$out->nama_tempat_wisata}}</span>
+    <div class="right floated meta">{{$out->nama_kota}}</div>
+    <br>
+    <img class="ui avatar image" src="{{asset($out->image) }}"> {{$out->name }}
+  </div>
+  <div class="image" >
+    <img src="{{asset($out->image) }}">
+  </div>
+  <div class="content">
+    <span class="right floated">
+      <i class="heart outline like icon"></i>
+      {{$out->like}} likes
+    </span>
     
+  </div>
+  <div class="extra content">
+    <div class="ui large transparent left icon input">
+      
+      <div class="description">
+      {{$out->caption}}
+    </div>
+    </div>
+  </div>
+</div>
+    
+@endforeach
+   
 
 
   </div>
+
+  
   
 </body>
     
